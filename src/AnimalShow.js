dda13bbab485b414773792da2fde8a607d1a352d
Ikamaz <mazmishviliika@gmail.com>
2024-07-25 03:30:46 +0400

    main commit

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,6 +1,26 @@
-function AnimalShow() {
+import bird from './img/bird.svg';
+import cat from './img/cat.svg';
+import cow from './img/cow.svg';
+import dog from './img/dog.svg';
+import gator from './img/gator.svg';
+import heart from './img/heart.svg';
+import horse from './img/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    heart,
+    horse
+}
+
+function AnimalShow({ type }) {
     return (
-        <div>Cow!</div>
+        <div>
+            <img src={svgMap[type]} alt="animal" />
+        </div>
     )
 }
 
